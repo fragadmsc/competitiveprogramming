ab1cce070a3f540c6dc740060886bcb24d4a4adf
fragadmsc <damasceno.lucas512@gmail.com>
2026-03-02 01:18:33 -0300

    added lis

---
+++ algorithms/other/lis.hpp
@@ -0,0 +1,18 @@
+#pragma once;
+
+#include <vector>
+
+template<typename T> 
+std::vector<T> lis(const std::vector<T>& vt) {
+    //returns a vector with the list of the given sequence
+    std::vector<T> stack;
+    for(int i=0; i<(int)size(vt); i++) {
+        std::vector<int>::iterator p = lower_bound(stack.begin(), stack.end(), vt[i]);
+        if(p == stack.end()) {
+            stack.pb(p);
+        } else {
+            *p = vt[i];
+        }
+    }
+    return stack;
+}
