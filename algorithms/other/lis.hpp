d22334b4d7019ea3cd1969f1b538c22f7bef5ce9
fragadmsc <damasceno.lucas512@gmail.com>
2026-03-13 15:38:29 -0300

    corrected lis function (it didn't even compile before

--- algorithms/other/lis.hpp
+++ algorithms/other/lis.hpp
@@ -1,17 +1,26 @@
-#pragma once;
+#pragma once
 
 #include <vector>
 
 template<typename T> 
-std::vector<T> lis(const std::vector<T>& vt) {
+std::vector<T> lis(const std::vector<T>& vt, bool strictly_increasing = true) {
     //returns a vector with the lis of the given sequence
     std::vector<T> stack;
     for(int i=0; i<(int)size(vt); i++) {
-        std::vector<int>::iterator p = lower_bound(stack.begin(), stack.end(), vt[i]);
-        if(p == stack.end()) {
-            stack.pb(p);
+        if(strictly_increasing) {
+            auto p = lower_bound(stack.begin(), stack.end(), vt[i]);
+            if(p == stack.end()) {
+                stack.push_back(vt[i]);
+            } else {
+                *p = vt[i];
+            }
         } else {
-            *p = vt[i];
+            auto p = upper_bound(stack.begin(), stack.end(), vt[i]);
+            if(p == stack.end()) {
+                stack.push_back(vt[i]);
+            } else {
+                *p = vt[i];
+            }
         }
     }
     return stack;
