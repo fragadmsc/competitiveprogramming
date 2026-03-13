34bdb3f03a515b8eac680386f0dff854349df1bd
fragadmsc <damasceno.lucas512@gmail.com>
2026-03-13 16:14:35 -0300

    corrected lis.cpp, now it is returning the right sequence, and not only the size of the right sequence

--- algorithms/other/lis.hpp
+++ algorithms/other/lis.hpp
@@ -1,27 +1,50 @@
 #pragma once
 
 #include <vector>
+#include <algorithm>
 
+/*returns a vector with the lis of the given sequence*/
 template<typename T> 
 std::vector<T> lis(const std::vector<T>& vt, bool strictly_increasing = true) {
-    //returns a vector with the lis of the given sequence
+    if(vt.empty()) return {};
+
+    int dist;
     std::vector<T> stack;
-    for(int i=0; i<(int)size(vt); i++) {
+    std::vector<int> stack_id;
+    std::vector<int> parent(vt.size(), -1);
+
+    for(int i = 0; i < std::size(vt); i++) {
+        
+        typename std::vector<T>::iterator p;
+        
         if(strictly_increasing) {
-            auto p = lower_bound(stack.begin(), stack.end(), vt[i]);
-            if(p == stack.end()) {
-                stack.push_back(vt[i]);
-            } else {
-                *p = vt[i];
-            }
+            p = std::lower_bound(stack.begin(), stack.end(), vt[i]);
         } else {
-            auto p = upper_bound(stack.begin(), stack.end(), vt[i]);
-            if(p == stack.end()) {
-                stack.push_back(vt[i]);
-            } else {
-                *p = vt[i];
-            }
+            p = std::upper_bound(stack.begin(), stack.end(), vt[i]);
+        }
+
+        dist = p - stack.begin();
+
+        if(p == stack.end()) {
+            stack.push_back(vt[i]);
+            stack_id.push_back(i);
+        } else {
+            *p = vt[i];
+            stack_id[dist] = i;
+        }
+
+        if(dist > 0) {
+            parent[i] = stack_id[dist-1];
         }
     }
-    return stack;
+
+    std::vector<T> ans;
+    int curr = stack_id.back();
+    while(curr != -1) {
+        ans.push_back(vt[curr]);
+        curr = parent[curr];
+    }
+    std::reverse(ans.begin(), ans.end());
+
+    return ans;
 }
