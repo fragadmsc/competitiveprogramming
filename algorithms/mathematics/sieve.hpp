2ccdbfc3cd744481a09f06b5eee6ad57f747e534
fragadmsc <damasceno.lucas512@gmail.com>
2026-03-16 19:56:18 -0300

    reduced memory allocation of sieve.hpp

--- algorithms/mathematics/sieve.hpp
+++ algorithms/mathematics/sieve.hpp
@@ -30,7 +30,7 @@ struct sieve {
     }
 
     /*returns a std::vector of pair(prime factor, exponent)*/
-    std::vector<pii> factorize(int x) {
+    const std::vector<pii>& factorize(int x) {
         if((int)prime_factorization.size() != 0) return prime_factorization[x];
 
         prime_factorization.resize(n+1);
@@ -38,19 +38,24 @@ struct sieve {
             if((int)prime_factorization[i].size() == 0) {
                 prime_factorization[i].push_back({i, 1});
                 for(int j = 2*i; j <= n; j += i) {
-                    int ax = 0, b = i;
-                    while(j%b == 0) {
+                    int ax = 0;
+                    int temp = j;
+                    while(temp%i == 0) {
                         ax++;
-                        b *= i;
+                        temp /= i;
                     }
                     prime_factorization[j].push_back({i, ax});
                 }
             }
         }
+        for(int i = 2; i <= n; i++) {
+            prime_factorization[i].shrink_to_fit();
+        }
+
         return prime_factorization[x];
     }
 
-    std::vector<int> divisors(int x) {
+    const std::vector<int>& divisors(int x) {
         if((int)divs.size() != 0) return divs[x];
 
         divs.resize(n+1);
@@ -58,6 +63,9 @@ struct sieve {
             for(int j = i; j <= n; j += i) {
                 divs[j].push_back(i);
             }
+        }
+        for(int i = 1; i<=n; i++) {
+            divs[i].shrink_to_fit();
         }
 
         return divs[x];
