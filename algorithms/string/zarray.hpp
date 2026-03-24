e622bcd81284840b4bd34389f965d6cb576cb368
fragadmsc <damasceno.lucas512@gmail.com>
2026-03-24 08:44:57 -0300

    added string overload for zarray

--- algorithms/string/zarray.hpp
+++ algorithms/string/zarray.hpp
@@ -3,6 +3,32 @@
 #include <vector>
 
 /*returns the zarray*/
+
+std::vector<int> zarray(string& vt) {
+    int n = (int)size(vt);
+    std::vector<int> z(n, 0);
+    int l = 0, r = 0;
+
+    for(int i = 1; i < n; i++) {
+        /*r is an inclusive boundary*/
+        if (i <= r) {
+            z[i] = std::min(r-i+1, z[i-l]);
+        }
+
+        while(i+z[i] < n && vt[i+z[i]] == vt[z[i]]) {
+            z[i]++;
+        }
+        
+        if(i+z[i]-1 > r) {
+            r = i+z[i]-1;
+            l = i;
+        }
+    }
+
+    return z;    
+}
+
+
 template<typename T>
 std::vector<int> zarray(const std::vector<T>& vt) {
     int n = (int)size(vt);
